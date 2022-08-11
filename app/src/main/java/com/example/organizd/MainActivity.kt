2a6da5e650b6755f249294c2e2fd11388041f2a4
EndriSefa <s1093017@studenti.univpm.it>
2022-08-11 16:57:41 +0200

    Commit nav bar funzionante

--- app/src/main/java/com/example/organizd/MainActivity.kt
+++ app/src/main/java/com/example/organizd/MainActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +20,18 @@ class MainActivity : AppCompatActivity() {
         val profileFragment = ProfileFragment()
 
         setCurrentFragment(homeFragment)
+
+        bottomNav = findViewById(R.id.bottomNavigationView) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.myHome ->setCurrentFragment(homeFragment)
+                R.id.myCalendar -> setCurrentFragment(calendarFragment)
+                R.id.myTimer -> setCurrentFragment(timerFragment)
+                R.id.myProfile -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
+
 
 
 
