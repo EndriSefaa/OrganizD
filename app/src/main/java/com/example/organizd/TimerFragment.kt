3af5f0fcd690a2666235fc90f8edfc039b727b58
GitAndreaHub <S1095511@studenti.univpm.it>
2022-08-13 12:16:18 +0200

    Toast message & eliminazione barra superiore

--- app/src/main/java/com/example/organizd/TimerFragment.kt
+++ app/src/main/java/com/example/organizd/TimerFragment.kt
@@ -1,6 +1,33 @@
 package com.example.organizd
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class TimerFragment : Fragment(R.layout.fragment_timer) {
+
+    //Toast.makeText(this@TimerFragment.requireActivity(), "saving", Toast.LENGHT_SHORT).show()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_timer, container, false)
+    }
+
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        var info = view.findViewById<ImageView>(R.id.infoIcon)
+
+        info.setOnClickListener{
+            Toast.makeText(this@TimerFragment.requireActivity(), "La funzione total focus permettere di silenziare qualunque notifica per migliorare la tua concentrazione.", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
+
