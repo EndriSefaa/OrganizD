d4e5067ece0f15266457aeb0a8278f68920a2e37
EndriSefa <s1093017@studenti.univpm.it>
2022-10-02 20:31:01 +0200

    Update Firebase

---
+++ app/src/main/java/com/example/organizd/db/entities/Song.kt
@@ -0,0 +1,8 @@
+package com.example.organizd.db.entities
+
+ data class Song(
+
+     val mediaId: String = "",
+     val title: String = "",
+     val songUrl: String =""
+ )
---
+++ app/src/main/java/com/example/organizd/other/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.organizd.other
+
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+}
