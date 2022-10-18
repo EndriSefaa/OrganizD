a59201cb537a09c74147eedc16186da948341617
GitAndreaHub <S1095511@studenti.univpm.it>
2022-10-18 12:48:42 +0200

    Sistemazione calendario

--- app/src/main/java/com/example/organizd/CalendarFragment.kt
+++ app/src/main/java/com/example/organizd/CalendarFragment.kt
@@ -1,6 +1,38 @@
 package com.example.organizd
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
 import androidx.fragment.app.Fragment
+import com.example.organizd.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment(R.layout.fragment_calendar) {
+
+    lateinit var binding: FragmentCalendarBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCalendarBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        //return super.onCreateView(inflater, container, savedInstanceState)
+
+
+        binding.calendarView.setOnDateChangeListener(CalendarView.OnDateChangeListener{view, year, month, dayOfMonth ->
+
+            val Date = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
+
+            binding.textCurrentDate.setText(Date)
+        })
+
+        return view
+    }
+
+
+
 }
